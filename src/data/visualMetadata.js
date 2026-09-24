a8f60672f327f03b2064e33bd014131c1f17cb12
Purvish69 <154321852+Purvish69@users.noreply.github.com>
2026-09-24 17:15:14 +0200

    Refactor code structure for improved readability and maintainability

--- src/data/visualMetadata.js
+++ src/data/visualMetadata.js
@@ -1,3 +1,13 @@
+import alexanderAlbonPortrait from '../assets/drivers/alexander-albon.webp';
+import arvidLindbladPortrait from '../assets/drivers/arvid-lindblad.webp';
+import carlosSainzPortrait from '../assets/drivers/carlos-sainz.png';
+import gabrielBortoletoPortrait from '../assets/drivers/gabriel-bortoleto.png';
+import isackHadjarPortrait from '../assets/drivers/isack-hadjar.png';
+import liamLawsonPortrait from '../assets/drivers/liam-lawson.webp';
+import nicoHulkenbergPortrait from '../assets/drivers/nico-hulkenberg.webp';
+import sergioPerezPortrait from '../assets/drivers/sergio-perez.png';
+import valtteriBottasPortrait from '../assets/drivers/valtteri-bottas.png';
+
 const mediaBase = 'https://media.formula1.com/image/upload';
 
 const teamSlug = { 'red_bull': 'redbullracing', 'rb': 'racingbulls', 'aston_martin': 'astonmartin', 'sauber': 'audi' };
@@ -8,25 +18,25 @@ const teamColors = { mercedes: '#00D2BE', ferrari: '#DC0000', mclaren: '#FF8000'
 const driverPortraits = {
   '1': 'https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/L/LANNOR01_Lando_Norris/lannor01.png.transform/12col/image.png',
   '3': 'https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/M/MAXVER01_Max_Verstappen/maxver01.png.transform/12col/image.png',
-  '5': 'https://media.formula1.com/image/upload/c_fill,w_720/q_auto/v1740000001/common/f1/2026/audi/gabbor01/2026audigabbor01right.webp',
-  '6': 'https://media.formula1.com/image/upload/c_fill,w_720/q_auto/v1740000001/common/f1/2026/redbullracing/isahad01/2026redbullracingisahad01right.webp',
+  '5': gabrielBortoletoPortrait,
+  '6': isackHadjarPortrait,
   '10': 'https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/P/PIEGAS01_Pierre_Gasly/piegas01.png.transform/12col/image.png',
-  '11': 'https://media.formula1.com/image/upload/c_fill,w_720/q_auto/v1740000001/common/f1/2026/cadillac/serper01/2026cadillacserper01right.webp',
+  '11': sergioPerezPortrait,
   '12': 'https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/K/ANDANT01_Kimi_Antonelli/andant01.png.transform/12col/image.png',
   '14': 'https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/F/FERALO01_Fernando_Alonso/feralo01.png.transform/12col/image.png',
   '16': 'https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/C/CHALEC01_Charles_Leclerc/chalec01.png.transform/12col/image.png',
   '18': 'https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/L/LANSTR01_Lance_Stroll/lanstr01.png.transform/12col/image.png',
   '22': 'https://www.formula1.com/content/dam/fom-website/drivers/Y/YUKTSU01_Yuki_Tsunoda/yuktsu01.png.transform/12col/image.png',
-  '23': 'https://media.formula1.com/image/upload/c_fill,w_720/q_auto/v1740000001/common/f1/2026/williams/alealb01/2026williamsalealb01right.webp',
-  '27': 'https://media.formula1.com/image/upload/c_fill,w_720/q_auto/v1740000001/common/f1/2026/audi/nichul01/2026audinichul01right.webp',
-  '30': 'https://media.formula1.com/image/upload/c_fill,w_720/q_auto/v1740000001/common/f1/2026/racingbulls/lialaw01/2026racingbullslialaw01right.webp',
+  '23': alexanderAlbonPortrait,
+  '27': nicoHulkenbergPortrait,
+  '30': liamLawsonPortrait,
   '31': 'https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/E/ESTOCO01_Esteban_Ocon/estoco01.png.transform/12col/image.png',
-  '41': 'https://media.formula1.com/image/upload/c_fill,w_720/q_auto/v1740000001/common/f1/2026/racingbulls/arvlin01/2026racingbullsarvlin01right.webp',
+  '41': arvidLindbladPortrait,
   '43': 'https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/F/FRACOL01_Franco_Colapinto/fracol01.png.transform/12col/image.png',
   '44': 'https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/L/LEWHAM01_Lewis_Hamilton/lewham01.png.transform/12col/image.png',
-  '55': 'https://media.formula1.com/image/upload/c_fill,w_720/q_auto/v1740000001/common/f1/2026/williams/carsai01/2026williamscarsai01right.webp',
+  '55': carlosSainzPortrait,
   '63': 'https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/G/GEORUS01_George_Russell/georus01.png.transform/12col/image.png',
-  '77': 'https://media.formula1.com/image/upload/c_fill,w_720/q_auto/v1740000001/common/f1/2026/cadillac/valbot01/2026cadillacvalbot01right.webp',
+  '77': valtteriBottasPortrait,
   '81': 'https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/O/OSCPIA01_Oscar_Piastri/oscpia01.png.transform/12col/image.png',
   '87': 'https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/O/OLIBEA01_Oliver_Bearman/olibea01.png.transform/12col/image.png'
 };
